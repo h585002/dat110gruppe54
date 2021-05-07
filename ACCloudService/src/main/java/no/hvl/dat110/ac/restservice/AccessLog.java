bd0561c346ec3346b6c170cfa1d5263a1f6ec073
h585002 <54101409+h585002@users.noreply.github.com>
2021-05-07 19:23:47 +0200

    Add files via upload

---
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -0,0 +1,45 @@
+package no.hvl.dat110.ac.restservice;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gson.Gson;
+
+public class AccessLog {
+	
+	private AtomicInteger cid;
+	protected ConcurrentHashMap<Integer, AccessEntry> log;
+	
+	public AccessLog () {
+		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
+		cid = new AtomicInteger(0);
+	}
+
+
+	public int add(String message) {
+		
+		int id = cid.getAndIncrement();
+		log.put(id, new AccessEntry(id, message));
+		
+		return id;
+	}
+		
+	public AccessEntry get(int id) {
+		
+		return log.get(id);
+		
+	}
+	
+
+	public void clear() {
+		log.clear();
+	}
+
+	public String toJson () {
+    	
+		
+		String json = new Gson().toJson(log.values().toArray());
+    	
+    	return json;
+    }
+}
